fdb15bfe4147334e811b164b07b530c1c4fa855c
Tochukwu Nwoko <nwokotochukwu@gmail.com>
2022-09-12 19:40:47 +0200

    Increase terraform + provider version

--- versions.tf
+++ versions.tf
@@ -16,9 +16,9 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = ">= 4.0"
     }
   }
 
-  required_version = "~> 1.0"
+  required_version = ">= 1.0"
 }
